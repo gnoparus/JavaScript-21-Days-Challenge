a51cc7962da310d1515b35bd8d370248d81ee448
gnoparus <surapong@gmail.com>
2020-04-11 18:49:39 +0700

    Finished ep.1

--- 01 - Falling Snow/start.js
+++ 01 - Falling Snow/start.js
@@ -8,24 +8,62 @@
     return {
       canvas, 
       canvasContext: canvas.getContext('2d'), 
-      numberOfSnowBalls: 250, 
+      numberOfSnowBalls: 300, 
     }
+  }
+  function random(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
   function createSnowBalls(canvas, numberOfSnowBalls) {
     const x = [...Array(numberOfSnowBalls)].map(() => {
       return {
-        x: 50, 
-        y: 50, 
+        x: random(0, canvas.width), 
+        y: random(0, canvas.height), 
+        radius: random(1, 3), 
+        opacity: random(0.3, 0.4), 
+        speedX: random(-3, 4), 
+        speedY: random(1, 3), 
+
       }
     })
-    console.log(x);
+    // console.log(x);
+    return x;
+  }
+
+  function drawSnowBall(canvasContext, snowBall) {
+    canvasContext.beginPath();
+    canvasContext.arc(snowBall.x, snowBall.y, snowBall.radius, 0, 2*Math.PI);
+    canvasContext.fillStyle = `rgba(255, 255, 255, ${snowBall.opacity})`;
+    canvasContext.fill();
+
+  }
+
+  function moveSnowBall(canvas, snowBall) {
+    snowBall.x += snowBall.speedX;
+    snowBall.y += snowBall.speedY;
+    
+    if (snowBall.x > canvas.width) {
+      snowBall.x = 0;
     }
+    else if (snowBall.x < 0) {
+      snowBall.x = canvas.width;
+    }
+    if (snowBall.y > canvas.height) {
+      snowBall.y = 0;
+    }
+  }
 
   function run() {
     const { canvas, canvasContext, numberOfSnowBalls } = setup();
-    createSnowBalls(canvas, numberOfSnowBalls);
-  
+    const snowBalls = createSnowBalls(canvas, numberOfSnowBalls);
+
+    setInterval(() => {
+      canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+      snowBalls.forEach((snowBall) => drawSnowBall(canvasContext, snowBall))
+      snowBalls.forEach((snowBall) => moveSnowBall(canvas, snowBall))
+
+    }, (50));
   }
   run();
 })();
