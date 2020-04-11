f5181931fce9058443c82e16792909e4489330c3
gnoparus <surapong@gmail.com>
2020-04-11 18:15:05 +0700

    Created fixed snowballs

--- 01 - Falling Snow/start.js
+++ 01 - Falling Snow/start.js
@@ -1,4 +1,31 @@
 (() => {
   // เริ่มเขียนโค้ด
+  function setup() {
+    const canvas = document.getElementById('falling-snow-canvas');
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
 
+    return {
+      canvas, 
+      canvasContext: canvas.getContext('2d'), 
+      numberOfSnowBalls: 250, 
+    }
+  }
+
+  function createSnowBalls(canvas, numberOfSnowBalls) {
+    const x = [...Array(numberOfSnowBalls)].map(() => {
+      return {
+        x: 50, 
+        y: 50, 
+      }
+    })
+    console.log(x);
+    }
+
+  function run() {
+    const { canvas, canvasContext, numberOfSnowBalls } = setup();
+    createSnowBalls(canvas, numberOfSnowBalls);
+  
+  }
+  run();
 })();
