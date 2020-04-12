23d98232b4594f843005ce0743f3273c0d478254
gnoparus <surapong@gmail.com>
2020-04-12 15:34:44 +0700

    Finished nested callbacks.

--- 03 - Async Await/start.js
+++ 03 - Async Await/start.js
@@ -2,7 +2,41 @@
   // เริ่มเขียนโค้ด
 
   // 1. How Asynchronous code works in JavaScript
+  function simulateAsyncAPI(text, timeout) {
+    setTimeout(() => {
+      console.log(text);
+    }, (timeout));
+  }
+
   // 2. Callback
+
+  function simulateAsyncAPI2(text, timeout, callback) {
+    setTimeout(() => {
+      console.log(text);
+      callback();
+    }, timeout);
+  }
   // 3. Promise
   // 4. Async/Await
+
+  function run() {
+    // // 1
+    // simulateAsyncAPI("A", 3000);
+    // simulateAsyncAPI("B", 2000);
+    // simulateAsyncAPI("C", 1000);
+
+    // 2
+    simulateAsyncAPI2("A", 3000, () => {
+      simulateAsyncAPI2("B", 2000, () => {
+        simulateAsyncAPI2("C", 1000, () => {
+          console.log("Callback")
+        })
+      })
+    });
+    
+    
+
+  }
+
+  run();
 })();
