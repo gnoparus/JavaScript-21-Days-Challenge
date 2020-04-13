18c1fbeed9e5ec230ed4a6ea5b537b671aff1f28
gnoparus <surapong@gmail.com>
2020-04-13 18:14:01 +0700

    Finished ep.7 konami code.

--- 07 - Konami Code/start.js
+++ 07 - Konami Code/start.js
@@ -1,4 +1,33 @@
 (() => {
   // เริ่มเขียนโค้ด
 
+  const KONAMI_CODE = [
+    "ArrowUp", "ArrowUp", 
+    "ArrowDown", "ArrowDown", 
+    "ArrowLeft", "ArrowRight", 
+    "ArrowLeft", "ArrowRight", 
+    "b", "a"];
+
+    let index = 0;
+
+  function onKeyDown(event) {
+    console.log(event.key);
+
+    if (event.key == KONAMI_CODE[index]) {
+      index += 1;
+      if (index === KONAMI_CODE.length) {
+        startSnowing();
+      }
+    }
+    else {
+      index = 0;
+    }
+  }
+
+  function run() {
+    // startSnowing();
+    document.addEventListener("keydown", onKeyDown);
+  }
+
+  run();
 })();
