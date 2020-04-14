6230a5b7af845cf2c42b904751a1da1af24ffdd4
gnoparus <surapong@gmail.com>
2020-04-14 15:06:01 +0700

    Finished ep.10 kanban board.

--- 10 - Kanban Board/start.js
+++ 10 - Kanban Board/start.js
@@ -1,4 +1,50 @@
 (() => {
   // เริ่มเขียนโค้ด
 
+  const taskElems = Array.from(document.querySelectorAll(".task"));
+  const dropZoneElems = Array.from(document.querySelectorAll(".drop-zone"));
+
+  let draggingElem;
+
+  function onDragStart(event) {
+    // console.log(event.target);
+    // console.log(this);
+    
+    // dragingElem = event.target;
+    draggingElem = this;
+  }
+
+  function onDrop(event) {
+    console.log("Drop");
+
+    // draggingElem.parent.removeChild(draggingElem);
+    this.append(draggingElem);
+    draggingElem = null;
+  }
+
+  function onDragOver(event) {
+    // console.log(event);
+    event.preventDefault();
+  }
+
+  function onDragEnter(event) {
+    // console.log(event);
+    event.preventDefault();
+  }
+
+  function run() {
+    // console.log(taskElems);
+
+    taskElems.forEach((taskElem) => {
+      taskElem.addEventListener("dragstart", onDragStart);
+    })
+
+    dropZoneElems.forEach((dropZoneElem) => {
+      dropZoneElem.addEventListener("drop", onDrop);
+      dropZoneElem.addEventListener("dragover", onDragOver);
+      dropZoneElem.addEventListener("dragenter", onDragEnter);
+    }) 
+  }
+
+  run();
 })();
