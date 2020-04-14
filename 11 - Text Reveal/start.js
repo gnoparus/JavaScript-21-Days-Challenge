9db6025a7bb6a9aa568ef7054e32da49adf44972
gnoparus <surapong@gmail.com>
2020-04-14 15:47:24 +0700

    Finished ep.11 Text Reveal.

--- 11 - Text Reveal/start.js
+++ 11 - Text Reveal/start.js
@@ -1,4 +1,33 @@
 (() => {
   // เริ่มเขียนโค้ด
 
+  const sectionElems = Array.from(document.querySelectorAll("section"));
+
+  function onScroll(event) {
+
+    const scrollPosition = window.pageYOffset;
+
+    sectionElems.forEach((sectionElem) => {
+      const imgElem = sectionElem.querySelector("img");
+      const textElem = sectionElem.querySelector(".text");
+
+      const revealPosition = imgElem.offsetTop + imgElem.offsetHeight / 10;
+
+      if (scrollPosition > revealPosition)  {
+        textElem.classList.add("reveal");
+      }
+      else {
+        textElem.classList.remove("reveal");
+      }
+    })
+    // console.log(`Scroll ${scrollPosition}`);
+  }
+
+  function run() {
+    console.log(sectionElems);
+    
+    document.addEventListener("scroll", onScroll);
+  }
+
+  run();
 })();
