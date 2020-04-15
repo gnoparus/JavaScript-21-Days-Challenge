86c10ca27d9e00f7b7149ba1a5636c143cf5ed04
gnoparus <surapong@gmail.com>
2020-04-15 20:02:06 +0700

    Finished ep.17 speech to text.

--- 17 - Speech to Text/start.js
+++ 17 - Speech to Text/start.js
@@ -1,3 +1,48 @@
 (() => {
   // เริ่มเขียนโค้ด
+
+  window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+  const recognition = new window.SpeechRecognition();
+  const btnElem = document.querySelector(".control");
+  const textElem = document.querySelector(".text");
+
+  function onClick(event) {
+
+    const isPausing = btnElem.classList.contains("record");
+
+    if (isPausing) {
+      recognition.start();
+      btnElem.classList.remove("record");
+      btnElem.classList.add("pause");
+      textElem.innerText = "";
+    } else {
+      recognition.stop();
+      btnElem.classList.add("record");
+      btnElem.classList.remove("pause");
+    }
+  }
+
+  function onResult(event) {
+
+    // console.log("onResult", event);
+    const transcript = event.results[0][0].transcript;
+
+    textElem.innerText += " " + transcript;
+    // console.log(transcript);
+  }
+
+  function onEnd() {
+    recognition.start();    
+  }
+
+  function run() {
+    recognition.lang = "en-US";
+
+    recognition.addEventListener("result", onResult);
+    recognition.addEventListener("end", onEnd);
+    btnElem.addEventListener("click", onClick);
+  }
+
+  run();
 })();
