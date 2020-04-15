90a53095822995a5345141d5dad9314e62b9d385
gnoparus <surapong@gmail.com>
2020-04-15 17:05:40 +0700

    Finished ep.16 text to speech.

--- 16 - Text to Speech/start.js
+++ 16 - Text to Speech/start.js
@@ -1,4 +1,29 @@
 (() => {
   // เริ่มเขียนโค้ด
 
+  const message = new SpeechSynthesisUtterance();
+  const imgElems = Array.from(document.querySelectorAll("img"));
+
+  function onVoicesChanged(event) {
+
+    const voices = speechSynthesis.getVoices();
+    const voice = voices.find(voice => voice.lang === "th-TH");
+    message.voice = voice;
+    // console.log(voices);
+  }
+
+  function onClick(event) {
+    // console.log(event.target);
+
+    message.text = event.target.getAttribute("alt");
+    speechSynthesis.speak(message);
+  }
+
+  function run() {
+    speechSynthesis.addEventListener("voiceschanged", onVoicesChanged);
+    imgElems.forEach(imgElem => { imgElem.addEventListener("click", onClick)});
+  }
+
+  run();
+
 })();
